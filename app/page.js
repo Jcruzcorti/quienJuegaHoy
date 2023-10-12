90781f65f723952057da9ba8d1dc82d8d3300c13
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2023-10-12 13:45:40 -0300

    We create DataMatchList and DataMatchItem components that will contain the data fetching and the data map

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import Image from 'next/image'
 import styles from './page.module.css'
 import ButtonTest from '@/components/buttons/ButtonTest'
+import DataMatchList from '@/components/data/DataMatchList'
 
 
 export default function Home() {
   return (
-    <main className={styles.main}>
-      <ButtonTest/>
-      <h2>BIENVENIDOS A QUIEN JUEGA HOY</h2>
-    </main>
+      <main className={styles.main}>
+        <ButtonTest/>
+        <h2>BIENVENIDOS A QUIEN JUEGA HOY</h2>
+        <DataMatchList/>
+      </main>
   )
 }
---
+++ components/data/DataMatchItem.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+
+
+function DataMatchItem({data}) {
+
+
+
+  return (
+    <>
+        {/* aca se va a mapear los partidos del día */}
+    </>
+  )
+}
+
+export default DataMatchItem
