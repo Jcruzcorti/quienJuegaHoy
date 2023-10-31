16d3f99aa665cd31cf2db90d7b05968726547036
Juan Cruz Cortiñas <juancruzcor@hotmail.com>
2023-10-31 17:46:28 -0300

    We changed the font color in horario argentino, and structured in a more centered way the components of the Main in mobile version

--- components/informationAlert/InformationAlert.jsx
+++ components/informationAlert/InformationAlert.jsx
@@ -48,7 +48,7 @@ export default function InformationAlert() {
         <DialogContent>
           <DialogContentText id="alert-dialog-slide-description">
             <Typography variant="subtitle" gutterBottom color='#828282'>
-                Los partidos disponibles corresponden a la primera división de Inglaterra, Italia, España, Alemania, Francia, Portugal y Brasil. Además, partidos del mundial, Champions League y Eurocopa
+                Los partidos disponibles corresponden a la primera división de Inglaterra, Italia, España, Alemania, Francia, Portugal y Brasil. Además, partidos del mundial, Champions League y Eurocopa.
             </Typography>
           </DialogContentText>
         </DialogContent>
